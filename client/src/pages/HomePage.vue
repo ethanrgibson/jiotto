7e6894c0c48315af6c49d653c6e874d1ce58ceec
ethanrgibson <ethanrgibson33@gmail.com>
2025-07-31 14:37:57 -0600

    correct and incorrect words and boxes changing based on guess

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,19 +1,24 @@
 <script setup>
 import { logger } from '@/utils/Logger.js';
 
-let editableGuessData = ''
-
-let secretWordData = ''
+import { ref } from 'vue'
+let editableGuessData = ref('')
+let secretWordData = ref('')
+let match = ref(null)
 
 function chooseSecretWord() {
-  logger.log(secretWordData)
+  logger.log(secretWordData.value)
 }
 
 function makeGuess() {
-  if (editableGuessData === secretWordData) {
+  if (editableGuessData.value === secretWordData.value) {
     logger.log('correct!')
+    match.value = true
   }
-  else (logger.log('incorrect!'))
+  else {
+    logger.log('incorrect!')
+    match.value = false
+  }
 }
 
 
@@ -24,28 +29,35 @@ function makeGuess() {
     <div class="row">
       <div class="col-12">
         <div class="text-center">
-          Guess The Secret Word!
-        </div>
-        <div>
-          <form @submit.prevent="makeGuess()">
-            <label for="guess">Your Guess</label>
-            <input v-model="editableGuessData" type="text" name="guess" id="userGuess" maxlength="5">
-            <div class="text-end m-4">
-              <button class="btn btn-purple" type="submit">Guess!</button>
-            </div>
-          </form>
-        </div>
-        <div class="text-center">
           Choose The Secret Word!
         </div>
         <div>
           <form @submit.prevent="chooseSecretWord()">
             <label for="guess">Your Word</label>
             <input v-model="secretWordData" type="text" name="guess" id="userGuess" maxlength="5">
-            <div class="text-end m-4">
+            <div class="text-center m-4">
               <button class="btn btn-purple" type="submit">Submit</button>
             </div>
           </form>
+        </div>
+        <div class="text-center">
+          Guess The Secret Word!
+        </div>
+        <div>
+          <form @submit.prevent="makeGuess()">
+            <label for="guess">Your Guess</label>
+            <input v-model="editableGuessData" type="text" name="guess" id="userGuess" maxlength="5">
+            <div class="text-center m-4">
+              <button class="btn btn-purple" type="submit">Guess!</button>
+            </div>
+          </form>
+        </div>
+        <div :style="{ backgroundColor: match ? 'green' : 'red' }">
+          <div>
+            <div class="display-5 text-center text-white">
+              {{ match ? 'Correct!' : 'Incorrect!' }}
+            </div>
+          </div>
         </div>
       </div>
     </div>
