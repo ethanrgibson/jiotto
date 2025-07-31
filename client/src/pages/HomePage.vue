345386031476ee286ac4a276f9c20db7ef9a143d
ethanrgibson <ethanrgibson33@gmail.com>
2025-07-31 13:55:25 -0600

    removed examples, getting things styled for guessing

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,29 @@
-<script setup >
-import Example from '@/components/Example.vue';
+<script setup>
+
 
 
 </script>
 
 <template>
-  <Example />
+  <div class="container-fluid home-container">
+    <div class="row">
+      <div class="col-12">
+        <div class="text-center">
+          Guess The Secret Word!
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-
+.home-container {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
